bbbbfe02eb47c8d9234b40a55b18a5bda5d4ed73
Jonathan Zacsh <jzacsh@gmail.com>
2017-06-21 20:10:35 -0400

    blackmagic of webdev testing toolchains
    Got through this black magic mostly from
    https://stackoverflow.com/a/42667840/287374 and its accompanying repo at
    https://github.com/SierraNL/webpack-typescript-jasmine/tree/a20c4bf0ca2c77 and
    some reading at https://github.com/TypeStrong/ts-loader
    
    note: these changes enabled last -- 7981f69ac7462 -- commit's tests to actually
    run (ie: these two commits are reversed in their order)

--- karma.conf.js
+++ karma.conf.js
@@ -1,23 +1,25 @@
+var webpackConf = require('./webpack.config');
+
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['karma-typescript', 'mocha', 'chai'],
-    files: [
-      'src/ts/*.spec.ts'
-    ],
-    exclude: [
-    ],
-    preprocessors: {
-      'src/ts/**.ts': ['karma-typescript']
-    },
+    frameworks: ['mocha', 'chai'],
+    files: ['src/ts/*.spec.ts'],
+    exclude: [],
+    preprocessors: {'src/ts/*.ts': ['webpack']},
     reporters: ['progress'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
+    mime: {'text/x-typescript': ['ts']},
     autoWatch: true,
     browsers: ['PhantomJS'],
     singleRun: false,
     concurrency: Infinity,
-    phantomjsLauncher: {exitOnResourceError: true}
+    phantomjsLauncher: {exitOnResourceError: true},
+    client: {captureConsole: true},
+    webpack: {
+      module: webpackConf.module,
+      resolve: webpackConf.resolve
+    }
   });
 };
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,21 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build/"),
-  }
+  },
+
+/* // TODO(jzacsh) determine if this can be deleted...
+  plugins: [
+    new LoaderOptionsPlugin({
+      debug: false,
+      options: {
+        resolve: {
+          extensions: ['.ts', '.tsx', '.js']
+        }
+      }
+    })
+  ],
+*/
+
+  resolve: { extensions: ['.ts', '.js', '.css'] },
+  module: {rules: [ {test: /\.ts$/, loader: 'ts-loader'}]}
 }
