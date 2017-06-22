7981f69ac7462dcc33303bed582d8d4f7c643a59
Jonathan Zacsh <jzacsh@gmail.com>
2017-06-21 20:10:01 -0400

    unit test: basic cases of building HTML

--- src/ts/fixture.domharness.ts
+++ src/ts/fixture.domharness.ts
@@ -1,25 +1,39 @@
 import { HumanMachine } from '../ts/flisting';
 
-export function buildRow(basen: string, size: HumanMachine,
-  date: HumanMachine): ((url: string) => string) {
+export interface rowBuilder {
+  (dir: string): string;
+}
 
-  return function(url: string) : string {
+export function buildRow(
+  basen: string, size: HumanMachine, date: HumanMachine): rowBuilder {
+
+  function anchorBuilder(path: string) : string {
+    return `
+    <a class="icon file" draggable="true" href="${path}/${basen}">${basen}</a>
+    `;
+  }
+  let sizeTdHtml : string = `
+    <td class="detailsColumn" data-value="${size.machine}">${size.human}</td>
+  `;
+  let dateTdHtml : string = `
+    <td class="detailsColumn" data-value="${date.machine}">${date.human}</td>
+  `;
+  return function(dir: string) : string {
+    let nameAnchorHtml = anchorBuilder(dir);
     return `
     <tr>
-    <td data-value="${basen}">
-      <a class="icon file" draggable="true" href="${url}/${basen}">${basen}</a>
-    </td>
-    <td class="detailsColumn" data-value="${size.machine}">${size.human}</td>
-    <td class="detailsColumn" data-value="${date.machine}">${date.human}</td>
+    <td data-value="${ basen }">${ nameAnchorHtml }</td>
+    ${ sizeTdHtml }
+    ${ dateTdHtml }
     </tr>
     `;
   };
 }
 
 export function buildTableStr(
-  url: string,
+  dir: string,
   id: string,
-  rows: Array<(url: string) => string>) : string {
+  rows: Array<rowBuilder>) : string {
   let html = `<table id="${ id }">`;
   html += `
 <thead>
@@ -32,13 +46,13 @@ export function buildTableStr(
 `;
   html += `<tbody>
 <tr>
-<td data-value=".."><a class="icon up" href="${ url }/..">[parent directory]</a></td>
+<td data-value=".."><a class="icon up" href="${ dir }/..">[parent directory]</a></td>
 <td class="detailsColumn" data-value="0"></td><td class="detailsColumn" data-value="0"></td>
 </tr>`;
-  for (let row in rows) {
-    html += row;
+  for (let i = 0; i < rows.length; ++i) {
+    html += rows[i](dir);
   }
-  html += `</tobdy></table>`;
+  html += `</tbody></table>`;
   return html;
 }
 
--- src/ts/flisting.spec.ts
+++ src/ts/flisting.spec.ts
@@ -4,31 +4,62 @@ import { expect } from 'chai';
 
 import * as mck from './fixture.domharness';
 
+const mockTableHtml = mck.buildTableStr('p/stuff/', 'MediaListing-vidoes', [
+  mck.buildRow("foo.html", mck.hm(123, '123 B'), mck.hm(432, '432 secs')),
+  mck.buildRow("bar.mp4", mck.hm(34809, '33 KB'), mck.hm(431, '400+ s. since epoch')),
+  mck.buildRow("thing.webm", mck.hm(13913, '14 KB'), mck.hm(194, '200+ s. since epoch')),
+]);
+
 describe('MediaListing of videos', () => {
-  let m;
+  let m : MediaListing;
+  let trs: NodeListOf<Element>;
   beforeEach(function() {
     // TODO(zacsh)rule out that `spyOn` isn't more appropriate here
-    document.body.insertAdjacentHTML('afterbegin', mck.buildTableStr(
-      'p/stuff/', 'MediaListing-vidoes', [
-        mck.buildRow("foo.html",
-          mck.hm(123, '123 B'),
-          mck.hm(432, '432 secs')),
-        mck.buildRow("bar.mp4",
-          mck.hm(34809, '33 KB'),
-          mck.hm(431, '400+ s. since epoch')),
-        mck.buildRow("thing.webm",
-          mck.hm(13913, '14 KB'),
-          mck.hm(194, '200+ s. since epoch')),
-    ]));
+    document.body.insertAdjacentHTML('afterbegin', mockTableHtml);
+    trs = document.querySelectorAll('table#MediaListing-vidoes tbody > tr');
+    expect(trs.length).to.equal(4); // including parent directory
+    m = new MediaListing(trs);
+  })
 
-    m = new MediaListing(document.querySelectorAll(
-      'table#MediaListing-vidoes tbody > tr'));
+  afterEach(function() {
+    let mockTableEl = document.getElementById('MediaListing-vidoes')
+    expect(mockTableEl, 'missing mock testbed').to.be.ok;
+    document.body.removeChild(mockTableEl!);
+  })
+
+  it('#listingSize getter', function() {
+    expect(m.listingSize).to.equal(3);
   });
-  let d = this;
-  it('#listingSize getter', function() { this.skip(); });
-  it('#length getter', function() { this.skip(); });
-  it('#isMixed getter', function() { this.skip(); });
-  it('get(...)', function() { this.skip(); });
+  it('#length getter', function() {
+    expect(m.length).to.equal(2); // bar.mp4 and thing.webm
+  });
+  it('#isMixed getter', function() {
+    expect(m.isMixed).to.equal(true); // foo.html is not AV
+  });
+
+  it('get(...).buildEl()', function() {
+    expect(m.length).to.equal(2); // sanity check
+
+    const barMp4El = m.get(0).buildEl();
+    expect(barMp4El.nodeName).to.equal('A');
+    expect(barMp4El.getAttribute('href')).to.equal('bar.mp4');
+    expect(barMp4El.children.length).to.equal(3); // TODO(zacsh) replace <br> with some CSS
+    expect(barMp4El.children[0].nodeName).to.equal('VIDEO');
+    expect(barMp4El.children[0].textContent).to.equal('Preview of bar.mp4');
+
+    expect(barMp4El.children[2].getAttribute('class')).to.equal('caption');
+    expect(barMp4El.children[2].textContent).to.equal('bar.mp4');
+
+    const thingWebmEl = m.get(1).buildEl();
+    expect(thingWebmEl.nodeName).to.equal('A');
+    expect(thingWebmEl.getAttribute('href')).to.equal('thing.webm');
+    expect(thingWebmEl.children.length).to.equal(3); // TODO(zacsh) replace <br> with some CSS
+    expect(thingWebmEl.children[0].nodeName).to.equal('VIDEO');
+    expect(thingWebmEl.children[0].textContent).to.equal('Preview of thing.webm');
+
+    expect(thingWebmEl.children[2].getAttribute('class')).to.equal('caption');
+    expect(thingWebmEl.children[2].textContent).to.equal('thing.webm');
+  });
 })
 
 describe('MediaListing of audio', () => {
