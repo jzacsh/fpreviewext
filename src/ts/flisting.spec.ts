1dca7756bd0fe3084c6196ce4005b8dd191685d0
Jonathan Zacsh <jzacsh@gmail.com>
2017-06-21 23:08:57 -0400

    feature(audio): include `.opus` file format
    also fixes copy/pasta typo of variable names from previous `describe` block

--- src/ts/flisting.spec.ts
+++ src/ts/flisting.spec.ts
@@ -68,15 +68,15 @@ describe('MediaListing of audio', () => {
   const mockTableHtml = mck.buildTableStr('p/stuff/', 'MediaListing-vidoes', [
     mck.buildRow("nils frahm.mp3", mck.hm(2851772, '2.8 M'), mck.hm(993, '993 secs')),
     mck.buildRow("thing.bats", mck.hm(34809, '33 KB'), mck.hm(2223, '223+ s. since epoch')),
-    mck.buildRow(
-      "keaton henson.flac", mck.hm(19768925, '19 M'), mck.hm(3999, '3999 s. since epoch')),
+    mck.buildRow("keaton.flac", mck.hm(19768925, '19 M'), mck.hm(3999, '3999 s. since epoch')),
+    mck.buildRow("henson.opus", mck.hm(17348923, '17 M'), mck.hm(3882, '3882 s. since epoch')),
   ]);
 
   let m : MediaListing;
   beforeEach(function() {
     document.body.insertAdjacentHTML('afterbegin', mockTableHtml);
     let trs = document.querySelectorAll('table#MediaListing-vidoes tbody > tr');
-    expect(trs.length).to.equal(4); // including parent directory
+    expect(trs.length).to.equal(5); // including parent directory
     m = new MediaListing(trs);
   })
 
@@ -87,12 +87,12 @@ describe('MediaListing of audio', () => {
   })
 
   it('#listingSize getter', function() {
-    expect(m.listingSize).to.equal(3);
+    expect(m.listingSize).to.equal(4);
   });
 
   it('#length getter', function() {
     expect(m.length,
-      'should have files "nils frahm.mp3" & "keaton henson.flac"').to.equal(2);
+      'should have files "nils frahm.mp3", "keaton.flac", "henson.opus"').to.equal(3);
   });
 
   it('#isMixed getter', function() {
@@ -105,23 +105,32 @@ describe('MediaListing of audio', () => {
   });
 
   it('get(...).buildEl()', function() {
-    const barMp4El = m.get(0).buildEl();
-    expect(barMp4El.nodeName).to.equal('A');
-    expect(barMp4El.getAttribute('href')).to.equal('nils frahm.mp3');
-    expect(barMp4El.children.length).to.equal(3); // TODO(zacsh) replace <br> with some CSS
-    expect(barMp4El.children[0].nodeName).to.equal('AUDIO');
-    expect(barMp4El.children[0].textContent).to.equal('Preview of nils frahm.mp3');
-    expect(barMp4El.children[2].getAttribute('class')).to.equal('caption');
-    expect(barMp4El.children[2].textContent).to.equal('nils frahm.mp3');
+    const nilsFrahmMp3El = m.get(0).buildEl();
+    expect(nilsFrahmMp3El.nodeName).to.equal('A');
+    expect(nilsFrahmMp3El.getAttribute('href')).to.equal('nils frahm.mp3');
+    expect(nilsFrahmMp3El.children.length).to.equal(3); // TODO(zacsh) replace <br> with some CSS
+    expect(nilsFrahmMp3El.children[0].nodeName).to.equal('AUDIO');
+    expect(nilsFrahmMp3El.children[0].textContent).to.equal('Preview of nils frahm.mp3');
+    expect(nilsFrahmMp3El.children[2].getAttribute('class')).to.equal('caption');
+    expect(nilsFrahmMp3El.children[2].textContent).to.equal('nils frahm.mp3');
 
-    const thingWebmEl = m.get(1).buildEl();
-    expect(thingWebmEl.nodeName).to.equal('A');
-    expect(thingWebmEl.getAttribute('href')).to.equal('keaton henson.flac');
-    expect(thingWebmEl.children.length).to.equal(3); // TODO(zacsh) replace <br> with some CSS
-    expect(thingWebmEl.children[0].nodeName).to.equal('AUDIO');
-    expect(thingWebmEl.children[0].textContent).to.equal('Preview of keaton henson.flac');
-    expect(thingWebmEl.children[2].getAttribute('class')).to.equal('caption');
-    expect(thingWebmEl.children[2].textContent).to.equal('keaton henson.flac');
+    const keatonFlacEl = m.get(1).buildEl();
+    expect(keatonFlacEl.nodeName).to.equal('A');
+    expect(keatonFlacEl.getAttribute('href')).to.equal('keaton.flac');
+    expect(keatonFlacEl.children.length).to.equal(3); // TODO(zacsh) replace <br> with some CSS
+    expect(keatonFlacEl.children[0].nodeName).to.equal('AUDIO');
+    expect(keatonFlacEl.children[0].textContent).to.equal('Preview of keaton.flac');
+    expect(keatonFlacEl.children[2].getAttribute('class')).to.equal('caption');
+    expect(keatonFlacEl.children[2].textContent).to.equal('keaton.flac');
+
+    const hensonOpusEl = m.get(2).buildEl();
+    expect(hensonOpusEl.nodeName).to.equal('A');
+    expect(hensonOpusEl.getAttribute('href')).to.equal('henson.opus');
+    expect(hensonOpusEl.children.length).to.equal(3); // TODO(zacsh) replace <br> with some CSS
+    expect(hensonOpusEl.children[0].nodeName).to.equal('AUDIO');
+    expect(hensonOpusEl.children[0].textContent).to.equal('Preview of henson.opus');
+    expect(hensonOpusEl.children[2].getAttribute('class')).to.equal('caption');
+    expect(hensonOpusEl.children[2].textContent).to.equal('henson.opus');
   });
 })
 
